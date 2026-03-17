9bbfb5344a0c63e792b09b30d67cc413f3f19e75
tommaso <tommy.agostini04@gmail.com>
2026-03-17 17:50:30 +0100

    Fix: mercato da telefono

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dbure75tr8g"
+    "revision": "0.6p7tjq3n4a8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
